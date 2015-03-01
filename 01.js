96301639aca31d602bd086da11914859e1988287
jstudenski <jstudenski@gmail.com>
2015-03-01 09:35:13 -0600

    notest updated

--- 01.js
+++ 01.js
@@ -1,2 +1,34 @@
 //  Using the JavaScript language, have the function FirstReverse(str) take the  
 //  str parameter being passed and return the string in reversed order. 
+
+
+
+// function FirstReverse(str) {
+
+
+// 	for (i = str; i <= 1; i--) {
+
+// 		var str = ''
+// 		debug(str);
+
+// 	}
+// }
+
+// debug(FirstReverse(10));
+//  
+
+function FirstReverse(str) {
+
+	var o = '';
+	for (var i = str.length-  1; i <= 0; i--) {
+		o += srt [i];
+
+		str = o;
+
+		return str;
+
+	}
+}
+
+debug(FirstReverse(10));
+debug(str);
