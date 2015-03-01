27ed3189637330b70f8617f613b1a8d3fabcfafc
jstudenski <jstudenski@gmail.com>
2015-03-01 10:07:27 -0600

    wrote out problem 28

--- 28.js
+++ 28.js
@@ -2,3 +2,20 @@
 // which will always be a positive integer and return its additive persistence which is the number of times you 
 // must add the digits in num until you reach a single digit. For example: if num is 2718 then your program should 
 // return 2 because 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9. 
+
+
+//make sure its a positive integer? 
+
+function AdditivePersistence(num) {
+
+var currentnum = num
+
+//while currentnum.length > 1 
+//do...
+	//split string ('')
+	//save variables
+	//add them togther
+	//answer = current num .. repeat
+//else return currentnum
+
+}
