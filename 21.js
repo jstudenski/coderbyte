cd9cb4dab1c96cae52c3895a97e3ee0bd4610d93
jstudenski <jstudenski@gmail.com>
2015-03-01 11:11:33 -0600

    more updates

--- 21.js
+++ 21.js
@@ -4,3 +4,9 @@
  // total number of minutes between the two times. The time will be in a 12 hour
  // clock format. For example: if str is 9:00am-10:00am then the output should be
  // 60.If str is 1:00pm-11:00am the output should be 1320. 
+
+
+function CountingMinutesI(str) {
+
+
+}
--- 22.js
+++ 22.js
@@ -1,6 +1,25 @@
-  // Using the JavaScript language, have the function MeanMode(arr) take the
+  // Usiwng the JavaScript language, have the function MeanMode(arr) take the
   // array of numbers stored in arr and return 1 if the mode equals the mean, 0
   // if they don't equal each other (ie. [5, 3, 3, 3, 1] should return 1
   // because the mode (3) equals the mean (3)). The array will not be empty,
   // will only contain positive integers, and will not contain more than one
   // mode. 
+
+
+function mode() {
+
+}
+
+function mean() {
+
+}
+
+function MeanMode(arr) {
+
+	if mean(arr) == mode(arr) {
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
