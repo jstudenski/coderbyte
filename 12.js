85effbd846d882969d2ca75bead8e7b9917fac58
jstudenski <jstudenski@gmail.com>
2015-03-02 09:30:08 -0600

    solved problem 12

--- 12.js
+++ 12.js
@@ -3,10 +3,25 @@
 // vowels the string contains (ie. "All cows eat grass" would return 5). 
 // Do not count y as a vowel for this challenge. 
 
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
 function VowelCount(str) {
 
 	count = 0;
 
-	// split('')
-	// if A E I O U count += 1
+	var myArray = str.split('');
+
+	for (i in myArray) {
+
+		if (vowels.indexOf(myArray[i]) >= 0) {
+
+			count += 1;
+		}
+	}
+
+	return count;
 }
+
+console.log(VowelCount("hello hello az AB! zzzzzzz; ; ;; 33"))
+
+
