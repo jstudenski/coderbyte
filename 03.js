46536820a1361ad6a138d1b1b3cf9416efbe5752
jstudenski <jstudenski@gmail.com>
2015-03-01 19:22:49 -0600

    Problem 3 test casses don't pass

--- 03.js
+++ 03.js
@@ -3,3 +3,30 @@
  // there are two or more words that are the same length, return the first word
  // from the string with that length.Ignore punctuation and assume sen will not
  // be empty. 
+
+
+function LongestWord(sen) { 
+
+	var myArray = sen.split(' ');
+	var longest = 0;
+	var longestword = '';
+	    myArray.forEach(function(i) {
+	        if (i.length >= longest) {
+	                longest = i.length;
+	                longestword = i;
+	        }
+	    });
+	    
+	    return longestword;        
+}
+   
+   
+if(LongestWord("hello jk jjjjjj afaew fwe af") == "jjjjjj") {console.log('passed')} else {console.log('failed')};
+
+if(LongestWord("hello world") == 'passed') {console.log('passed')} else {console.log('failed')};
+
+if(LongestWord("a beautiful sentence^&!") == 'beautiful') {console.log('passed')} else {console.log('failed')};
+
+if(LongestWord("letter after letter!!") == 'letter') {console.log('passed')} else {console.log('failed')};
+
+if(LongestWord("a confusing /:sentence:/[ this is not!!!!!!!~") == "confusing") {console.log('passed')} else {console.log('failed')};
